054e91ee68b84ce865c586b443ef5fea0df3e36a
Elmer Fiqi <61036187+Elmerf@users.noreply.github.com>
2022-06-24 16:04:06 +0700

    update path

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get('/redirect', async (req, res) => {
       }),
     });
     const data = JSON.stringify(await result.json());
-    fs.writeFileSync('./tmp/tokenCache.json', data);
+    fs.writeFileSync('../../tmp/tokenCache.json', data);
     res.redirect('/');
   }
 });
@@ -62,7 +62,7 @@ app.get('/redirect', async (req, res) => {
 app.get('/token', async (req, res) => {
   try {
     const url = new URL('https://login.microsoftonline.com/common/oauth2/v2.0/token');
-    const token = fs.readFileSync('./tmp/tokenCache.json', 'utf8');
+    const token = fs.readFileSync('../../tmp/tokenCache.json', 'utf8');
     const tokenParsed = JSON.parse(token);
 
     const result = await fetch(url, {
@@ -79,7 +79,7 @@ app.get('/token', async (req, res) => {
       }),
     });
     const data = JSON.stringify(await result.json());
-    fs.writeFileSync('./tmp/tokenCache.json', data);
+    fs.writeFileSync('../../tmp/tokenCache.json', data);
     res.send({ msg: 'TOKEN REFRESHED' });
   } catch (err) {
     console.log(err);
