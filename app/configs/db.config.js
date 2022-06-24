c4650395ca3dce9926eac65cc1b10967f4565ffe
Elmer Fiqi <61036187+Elmerf@users.noreply.github.com>
2022-06-24 14:32:52 +0700

    add dialectModule

--- app/configs/db.config.js
+++ app/configs/db.config.js
@@ -3,6 +3,7 @@
 // } = JSON.parse(process.env.DB_CREDENTIALS);
 
 // console.log(process.env.DB_CREDENTIALS);
+const pg = require('pg');
 
 module.exports = {
   // HOST,
@@ -11,6 +12,7 @@ module.exports = {
   // DB,
   PORT: 5432,
   dialect: 'postgres',
+  dialectModule: pg,
   pool: {
     max: 5,
     min: 0,
--- app/models/index.js
+++ app/models/index.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_URL,
   {
     dialect: config.dialect,
+    dialectModule: config.dialectModule,
     operatorsAliases: false,
     pool: {
       max: config.pool.max,
